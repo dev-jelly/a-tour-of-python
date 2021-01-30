a743cc6a5d198e859700afd0717def61b29cbfa7
jelly <chm073@gmail.com>
2021-01-30 17:57:14 +0900

    Fix url problem

--- src/utils/get-code.ts
+++ src/utils/get-code.ts
@@ -2,7 +2,7 @@ import { useAsync } from "react-use";
 
 export const useCode = (lang: string, hash: string) => {
   return useAsync(async () => {
-    const res = await fetch(`/articles/${lang}/${hash}.py`);
+    const res = await fetch(`./articles/${lang}/${hash}.py`);
     if (!res.ok) throw new Error(res.statusText);
     return await res.text();
   }, [lang, hash]);
--- src/utils/get-markdown.ts
+++ src/utils/get-markdown.ts
@@ -2,7 +2,7 @@ import { useAsync } from "react-use";
 
 export const useMarkdown = (lang: string, hash: string) => {
   return useAsync(async () => {
-    const res = await fetch(`/articles/${lang}/${hash}.md`);
+    const res = await fetch(`./articles/${lang}/${hash}.md`);
     if (!res.ok) throw new Error(res.statusText);
     return await res.text();
   }, [lang, hash]);
